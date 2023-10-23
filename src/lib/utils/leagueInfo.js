733d400a4733cc6cbfa0ae0ff8bd9588d7fefc9e
ORido00 <148760923+ORido00@users.noreply.github.com>
2023-10-23 13:58:27 +0100

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -9,7 +9,7 @@ export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VI
 export const homepageText = `
   <p>Founded in the depths of a 2021 Covid lockdown, Live Nasty Dy Nasty is an NFL Fantasy Football dynasty league with a British twist.</p>
   <p>Many players had been playing redraft for years and had grown a love for the game. This led to co-commissioners Rid and Miles deciding it was time to take the plunge into full NFL nerd-dom and commit to a dynasty league. The bat signal went out and 9 of the 12 places were filled immediately from their previous league. The final three spots were quickly filled by Mikey, Rhys and, the league's only American, Brad (USA! USA! USA!). It's quite a group they were joining, but it says a lot about them that they fit right in.</p>
-  <p>The research was done, the votes were in and the league took shape. A true democracy, this would be a full PPR Superflex league, with three IDP spots per team for DL, LB and DB. Oh, and kickers were kept in for some reason.</p>
+  <p>The research was done, the votes were in and the league took shape. A true democracy, a league charter was created. This would be a full PPR Superflex league, with three IDP spots per team for DL, LB and DB. Oh, and kickers were kept in for some reason.</p>
   <p>The start-up draft happened over two glorious evenings spent over zoom calls. We saw different tactics and different approaches, but it was clear everyone had put in the work and were fully committed to the league. The competition had started and long may it continue.</p>
   <p>Expect a lot of shit talk, shit teams and a few shit managers. Welcome to Live Nasty, Dy Nasty.</p>
 `;
